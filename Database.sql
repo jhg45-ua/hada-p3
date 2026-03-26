aabbaefb074cb89c7b35a60d81f920b0f6bfdb4f
jhg45-ua <jhg45@alu.ua.es>
2026-03-26 20:04:01 +0100

    48795869N Base de datos creada y consulta guardada

---
+++ Database.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Categories]
+(
+	[id] INT IDENTITY (1, 1) NOT NULL,
+	[name] NVARCHAR (32) NOT NULL,
+	PRIMARY KEY CLUSTERED ([id] ASC)
+)
+
+CREATE TABLE [dbo].[Products]
+(
+	[id] INT IDENTITY (1, 1) NOT NULL,
+	[name] NVARCHAR (32) NOT NULL,
+	[code] NVARCHAR (16) NOT NULL,
+	[amount] INT NOT NULL,
+	[price] FLOAT NOT NULL,
+	[category] INT NOT NULL,
+	[creationDate] DATETIME NOT NULL,
+	PRIMARY KEY CLUSTERED ([id] ASC),
+	UNIQUE NONCLUSTERED ([code] ASC),
+	constraint FK_Products_category foreign key (category) references Categories(id)
+)
+
+INSERT INTO [dbo].[Categories] VALUES
+('Computing'), ('Telephony'), ('Gaming'), ('Home appliances');
