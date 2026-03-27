29b740948c128a1e9294d3d9f4811f86499e395b
Ximamon44 <ximamon44@gmail.com>
2026-03-27 10:08:17 +0100

    Implementación de ENCategory.cs

--- library/ENCategory.cs
+++ library/ENCategory.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace library
 {
     internal class ENCategory
     {
+        public int Id;
+        public string Name;
+
+        public ENCategory()
+        {
+        }
+
+        public ENCategory(int id)
+        {
+            Id = id;
+        }
+
+        public ENCategory(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public bool Read()
+        {
+            return new CADCategory().read(this);
+        }
+
+        public List<ENCategory> ReadAll()
+        {
+            return new CADCategory().readAll();
+        }
     }
 }
